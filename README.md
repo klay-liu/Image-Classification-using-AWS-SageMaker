901ddd30a8a19fe3a5e8e2607152da2625fd8f78
Liu <zongyliu@amazon.com>
2022-10-25 23:13:55 +0800

    version 2 for hpo, train, and deployment'

--- README.md
+++ README.md
@@ -3,56 +3,91 @@ This project uses the dog breed classification data set to train a pretrained mo
 
 ## Project Set Up and Installation
 Enter AWS through the gateway in the course and open SageMaker Studio. 
-Download the starter files.
+Download the [starter files](https://github.com/udacity/CD0387-deep-learning-topics-within-computer-vision-nlp-project-starter).
 Download/Make the dataset available. 
 
 ## Dataset
-[dogbreed classification dataset](https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/dogImages.zip).
+[Dog Breed Classification Dataset](https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/dogImages.zip).
 
 
 ### Access
-Upload the data to an S3 bucket through the AWS Gateway so that SageMaker has access to the data. 
+You can download the data for [dog breed](https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/dogImages.zip), unzip the files and upload it to an S3 bucket so that SageMaker has access to the data. 
+
+## Script Files used
+
+1. `hpo.py` for hyperparameter tuning jobs where we train the model for multiple time with different hyperparameters and search for the best one based on loss metrics.
+2. `train_model.py` for really training the model with the best parameters getting from the previous tuning jobs, and put debug and profiler hooks for debugging purpose.
+3. `inference.py` to using the trained model as inference and post-processing and serializing the data before it passes to the endpoint for prediction.
+4. `train_and_deploy.ipynb` is a submission script that can create Hyperparameter Tuning job, Training job and Endpoints.
 
 ## Hyperparameter Tuning
 What kind of model did you choose for this experiment and why? Give an overview of the types of parameters and their ranges used for the hyperparameter search
-I choose the VGG19 fro this experiment since it is an advanced CNN with pre-trained layers and a great understanding of what defines an image in terms of shape, color, and structure. 
+I choose the VGG19 for this experiment since it is an advanced CNN with pre-trained layers and a great understanding of what defines an image in terms of shape, color, and structure.  
 
-**1. Screenshot of completed training jobs **![hpo.png](https://d-ehlicnlmgqny.studio.us-east-1.sagemaker.aws/jupyter/default/files/soothsayerworkflow/ref2/project/image_classification_project/CD0387-deep-learning-topics-within-computer-vision-nlp-project-starter/hpo.png?_xsrf=2%7Cf17ea0a5%7C59b349f0f19a4676c2f89ae3150c860f%7C1664774910)
-**2. Logs metrics during the training process**
+![hyperparameter-tuning](images/hyperparameter-tuning.png)
 
-**3. Tune at least two hyperparameters**
+
+
+Below is the metrics during the training process in log file for the best estimator:
+
+![hpo-best-estimator-log](images/hpo-best-estimator-log.png)
+
+We select `lr` and `batch_size` as the hyperparameters: 
+
+```
 hyperparameter_ranges = {
     "lr": ContinuousParameter(0.001, 0.1),
-    "train_batch_size": CategoricalParameter([32, 64, 128, 256, 512]),
+    "batch_size": CategoricalParameter([32, 64, 128]),
 }
-**4. Retrieve the best best hyperparameters from all your training jobs**
-{'_tuning_objective_metric': '"average test loss"',
- 'lr': '0.004277615192677857',
- 'momentum': '0.9',
- 'num_classes': '133',
- 'sagemaker_container_log_level': '20',
- 'sagemaker_estimator_class_name': '"PyTorch"',
- 'sagemaker_estimator_module': '"sagemaker.pytorch.estimator"',
- 'sagemaker_job_name': '"pytorch-training-2022-10-21-10-01-17-755"',
- 'sagemaker_program': '"hpo.py"',
- 'sagemaker_region': '"us-east-1"',
- 'sagemaker_submit_directory': '"s3://sagemaker-deployment-project/model-artifacts/pytorch-training-2022-10-21-10-01-17-755/source/sourcedir.tar.gz"',
- 'test_batch_size': '128',
- 'train_batch_size': '"512"'}
+```
+
+Below is the best hyperparameters from all the training jobs:
+
+```
+best_hyperparameters={
+    'lr': 0.012674954748888564,
+    'batch_size': 64,
+}
+```
+
+![best-hyperparameters](images/best-hyperparameters.png)
 
 ## Debugging and Profiling
-**TODO**: Give an overview of how you performed model debugging and profiling in Sagemaker
+We set the Debugger Hook to visualize the Loss Criterion of the process in both training and validation/testing. The Plot of the *Cross Entropy Loss* is shown below:
+
+![debugging-output](images/debugging-output.png)
+
+Profiling report see in `profiler-report.html`.
 
 ### Results
-**TODO**: What are the results/insights did you get by profiling/debugging your model?
+The CrossEntropyLoss_output_0 during training decreases and the  val_CrossEntropyLoss_output_0 doesn't increase, so no overfitting recorded.
 
-**TODO** Remember to provide the profiler html/pdf file in your submission.
+During our training job, the LowGPUUtilization rule was the most frequently triggered. It processed 1068 datapoints and was triggered 4 times.
 
 
 ## Model Deployment
-**TODO**: Give an overview of the deployed model and instructions on how to query the endpoint with a sample input.
+We use a separate script for inference in `inference.py` which implements the `model_fn`, `input_fn` and `predict_fn`. In `input_fn`, we only accept `image/jpeg` and `application/json` two types of content_type for the request. Model was deployed to a "ml.g4dn.xlarge" instance type.
 
-**TODO** Remember to provide a screenshot of the deployed active endpoint in Sagemaker.
+How to query the endpoint with a sample image:
 
-## Standout Suggestions
-**TODO (Optional):** This is where you can provide information about any standout suggestions that you have attempted.
+```
+# open an image as bytetype object  
+with open("./dogImages/test/008.American_staffordshire_terrier/American_staffordshire_terrier_00538.jpg", "rb") as f:
+    payload = f.read()
+# pass it to the endpoint 
+response=predictor.predict(payload, initial_args={"ContentType": "image/jpeg"})
+
+print(f'The prediction label: {np.argmax(response, 1)[0]}')
+print(f'prediction:\n {response}')
+
+```
+
+
+
+Below is the endpoint we deployed:
+
+![endpoint](images/endpoint.png)
+
+Prediction result with sample image:
+
+![prediction-result](images/prediction-result.png)
--- hpo.py
+++ hpo.py
@@ -18,63 +18,75 @@ from torchvision.datasets import ImageFolder
 from PIL import ImageFile, Image
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logger.addHandler(logging.StreamHandler(sys.stdout))
 
-#TODO: Import dependencies for Debugging andd Profiling
-    
 def test(model, test_loader, criterion, device):
-    '''
-    TODO: Complete this function that can take a model and a 
-          testing data loader and will get the test accuray/loss of the model
-          Remember to include any debugging/profiling hooks that you might need
-    '''
-    model.eval()
-    test_loss = 0
-    correct = 0
-    with torch.no_grad():
-        for data, target in test_loader:
-            data = data.to(device)
-            target = target.to(device)
-            output = model(data)
-            test_loss += criterion(output, target).item()  # sum up batch loss
-            pred = output.max(1, keepdim=True)[1]  # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
-
-    test_loss /= len(test_loader.dataset)
-    logger.info(
-        "Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
-            test_loss, correct, len(test_loader.dataset), 100.0 * correct / len(test_loader.dataset)
-        )
-    )
-
-def train(model, train_loader, criterion, optimizer, device):
-    '''
-    TODO: Complete this function that can take a model and
-          data loaders for training and will get train the model
-          Remember to include any debugging/profiling hooks that you might need
-    '''
-    model.train()
-    for batch_idx, (data, target) in enumerate(train_loader, 1):
+    model.eval() 
+    
+    losses = []
+    corrects=0  
+    
+    for data, labels in test_loader:
         data = data.to(device)
-        target = target.to(device)
-        optimizer.zero_grad()
-        output = model(data)
-        loss = criterion(output, target)
-        loss.backward()
-        optimizer.step()
-        if batch_idx % 100 == 0:
-            logger.info(
-                "Train progress: [{}/{} ({:.0f}%)] Loss: {:.6f}".format(
-                    batch_idx * len(data),
-                    len(train_loader.dataset),
-                    100.0 * batch_idx / len(train_loader),
-                    loss.item(),
-                )
-            )
+        labels = labels.to(device)
+        outputs=model(data)
+        loss=criterion(outputs, labels)
+        _, preds = torch.max(outputs, 1)
+        losses.append(loss.item())             # calculate running loss
+        corrects += torch.sum(preds == labels.data)     # calculate running corrects
 
+    avg_loss = np.mean(losses)
+    avg_acc = np.true_divide(corrects.double().cpu(), len(test_loader.dataset))
+    
+    logger.info('Testing Loss: {:.4f}, Accuracy: {:.4f}'.format(avg_loss, avg_acc)) # print the avg loss and accuracy values
+    
+def train(model, train_loader, valid_loader, criterion, optimizer, device, epochs):
+
+    best_loss = np.Inf #initialize best loss to infinity
+    dataset_dict={'train':train_loader, 'valid':valid_loader}
+    
+    for epoch in range(1, epochs+1):
+        logger.info(f"Epoch:{epoch}---")
+        
+        for phase in ['train', 'valid']:
+            if phase == 'train':
+                model.train()
+            else:
+                model.eval()
+                
+            losses = []
+            corrects = 0
+            
+            for data, labels in dataset_dict[phase]:
+                data = data.to(device)
+                labels = labels.to(device)
+                outputs = model(data)
+                loss = criterion(outputs, labels)
+                
+                if phase == 'train':
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+                
+                _, preds = torch.max(outputs, 1)
+                
+                losses.append(loss.item())
+                corrects += torch.sum(preds == labels.data)
+
+            avg_loss = np.mean(losses)
+            avg_acc = np.true_divide(corrects.cpu(), len(dataset_dict[phase].dataset))
+            
+            if phase=='valid':
+                if avg_loss < best_loss:
+                    best_loss = avg_loss
+            
+            logger.info('{} Loss: {:.4f}, Accuracy: {:.4f}, Best Loss: {:.4f}'.format(phase.capitalize(),
+                                                                                 avg_loss,
+                                                                                 avg_acc,
+                                                                                 best_loss))
+    return model
     
 def net(num_classes):
     vgg = models.vgg19(pretrained=True)
@@ -87,12 +99,10 @@ def create_data_loaders(data, batch_size):
     This is an optional function that you may or may not need to implement
     depending on whether you need to use data loaders or not
     '''
-    pass
-
-def _get_train_data_loader(batch_size, training_dir):
-    logger.info("Get train data loader")
-    # logger.info(f"{os.listdir(training_dir)}")
-    # train_path = os.path.join(training_dir, 'train')
+    train_data_path = os.path.join(data, 'train') # dogImages/train
+    test_data_path = os.path.join(data, 'test') # dogImages/test
+    valid_data_path = os.path.join(data, 'valid') # dogImages/valid
+    
     transform = transforms.Compose([
         transforms.Resize((120, 120)),
         transforms.ToTensor(),
@@ -100,110 +110,85 @@ def _get_train_data_loader(batch_size, training_dir):
     ])
 
     train_loader = DataLoader(
-        ImageFolder(training_dir, transform=transform),
+        ImageFolder(train_data_path, transform=transform),
         batch_size=batch_size, shuffle=True)
-    return train_loader
-    
-def _get_test_data_loader(batch_size, testing_dir):
-    logger.info("Get test data loader")
-    # logger.info(f"{os.listdir(testing_dir)}")
-    # test_path = os.path.join(training_dir, 'valid')
-   
-    transform = transforms.Compose([
-        transforms.Resize((120, 120)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=(0,0,0), std=(1,1,1))
-    ])
-
     test_loader = DataLoader(
-        ImageFolder(testing_dir, transform=transform),
+        ImageFolder(test_data_path, transform=transform),
         batch_size=batch_size, shuffle=True)
-    return test_loader
-
-def model_fn(model_dir):
-    model = net(num_classes=133)
-    with open(os.path.join(model_dir, "model.pth"), "rb") as f:
-        model.load_state_dict(torch.load(f))
-    return model
+    valid_loader = DataLoader(
+        ImageFolder(valid_data_path, transform=transform),
+        batch_size=batch_size, shuffle=True)
+    
+    return train_loader, test_loader, valid_loader
 
 def save_model(model, model_dir):
-    logger.info("Saving the model.")
+    logger.info("Saving the model...")
     path = os.path.join(model_dir, "model.pth")
     torch.save(model.cpu().state_dict(), path)
 
-    
 def main(args):
-    '''
-    TODO: Initialize a model by calling the net function
-    '''
+    
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    
+    logger.info(f"Running on Device {device}")
+    logger.info(f'Hyperparameters are LR: {args.lr}, Batch Size: {args.batch_size}')
+    logger.info(f'Data Path: {args.data_dir}')
+    
     model=net(args.num_classes)
-    logger.info(f'model: {model}')
     model = model.to(device)
     
-    '''
-    TODO: Create your loss and optimizer
-    '''
     loss_criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    # optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
     
-    '''
-    TODO: Call the train function to start training your model
-    Remember that you will need to set up a way to get training data from S3
-    '''
-    train_loader = _get_train_data_loader(args.train_batch_size, args.data_dir)
+    train_loader, test_loader, valid_loader = create_data_loaders(args.data_dir, args.batch_size)
+
     
-    train(model, train_loader, loss_criterion, optimizer, device)
+    logger.info("Training model...")
+    model=train(model, train_loader, valid_loader, loss_criterion, optimizer, device, args.epochs)
     
-    '''
-    TODO: Test the model to see its accuracy
-    '''
-    test_loader = _get_test_data_loader(args.test_batch_size, args.testing_data_dir)
+    logger.info("Testing the model...")
     test(model, test_loader, loss_criterion, device)
     
-    '''
-    TODO: Save the trained model
-    '''
+    # Saving the model
     save_model(model, args.model_dir)
 
 if __name__=='__main__':
     parser=argparse.ArgumentParser()
-    '''
-    TODO: Specify any training args that you might need
-    '''
+
     # Data and model checkpoints directories
     parser.add_argument(
-        "--train_batch_size",
+        "--batch_size",
         type=int,
         default=64,
         metavar="N",
         help="input batch size for training (default: 64)",
     )
     parser.add_argument(
-        "--test_batch_size",
+        "--epochs",
         type=int,
-        default=1000,
+        default=2,
         metavar="N",
-        help="input batch size for testing (default: 1000)",
+        help="epochs (default: 2)",
     )
     parser.add_argument(
         "--num_classes",
         type=int,
-        default=2,
+        default=133,
         metavar="N",
-        help="number of classes (default: 2)",
+        help="number of classes (default: 133)",
     )
     parser.add_argument(
         "--lr", type=float, default=0.01, metavar="LR", help="learning rate (default: 0.01)"
     )
-    parser.add_argument(
-        "--momentum", type=float, default=0.5, metavar="M", help="SGD momentum (default: 0.5)"
-    )
+    # parser.add_argument(
+    #     "--momentum", type=float, default=0.5, metavar="M", help="SGD momentum (default: 0.5)"
+    # )
 
     # Container environment
     parser.add_argument("--model_dir", type=str, default=os.environ["SM_MODEL_DIR"])
     parser.add_argument("--data_dir", type=str, default=os.environ["SM_CHANNEL_TRAIN"])
-    parser.add_argument("--testing_data_dir", type=str, default=os.environ["SM_CHANNEL_TEST"])
+    parser.add_argument('--output_dir', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
     
     args=parser.parse_args()
     
